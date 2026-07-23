ab0c33c3eeef96427594871bd1d4a2c53c2a96ab
eo404 <eldhox010@gmail.com>
2026-07-24 00:32:20 +0530

    feat(course): add course module structure

---
+++ backend/src/controllers/course.controller.js
@@ -0,0 +1,9 @@
+export const createCourse = () => {};
+
+export const getAllCourses = () => {};
+
+export const getCourseById = () => {};
+
+export const updateCourse = () => {};
+
+export const deleteCourse = () => {};
